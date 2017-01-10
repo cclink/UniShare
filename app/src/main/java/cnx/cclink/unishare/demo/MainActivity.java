8c5d48d29d3296e9d00c3b7d1945d5577f1dc3e4
zjn0645 <zhome90@163.com>
2017-01-10 16:44:01 +0800

    增加测试代码

--- app/src/main/java/cnx/cclink/unishare/demo/MainActivity.java
+++ app/src/main/java/cnx/cclink/unishare/demo/MainActivity.java
@@ -1,7 +1,15 @@
 package cnx.cclink.unishare.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import cnx.cclink.unishare.ShareApi;
+import cnx.cclink.unishare.ShareContent;
+import cnx.cclink.unishare.ShareError;
+import cnx.cclink.unishare.ShareListener;
+import cnx.cclink.unishare.SharePlatform;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button shareBtn = (Button) findViewById(R.id.shareBtn);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShareApi.share(MainActivity.this, SharePlatform.QQ, new ShareContent("标题", "内容", "", "https://github.com/cclink"), new ShareListener() {
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(ShareError shareError) {
+
+                    }
+                });
+            }
+        });
     }
 }

0e243df96811e0af9dcb0c6bd366bfbabe89c277
zjn0645 <zhome90@163.com>
2017-02-08 15:06:41 +0800

    更新QQ SDK，去掉原先的解决QQ分享后存在一个透明Activity问题的方式

--- unishare_sdk/src/main/java/cnx/cclink/unishare/platform/QQShareActivity.java
+++ unishare_sdk/src/main/java/cnx/cclink/unishare/platform/QQShareActivity.java
@@ -44,8 +44,6 @@ public class QQShareActivity extends AppCompatActivity {
     private String mShareImageFile;
     private String mShareURL;
     private boolean mIsTimeLine;
-    private boolean mIsQQStart;
-    private boolean mCancelAnyway;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,84 +57,12 @@ public class QQShareActivity extends AppCompatActivity {
         mShareImageFile = intent.getStringExtra("imageFile");
         mShareURL = intent.getStringExtra("shareURL");
         mIsTimeLine = intent.getBooleanExtra("isTimeline", false);
-        mIsQQStart = false;
-        mCancelAnyway = false;
-    }
-
-    @Override
-    protected void onStart() {
-        // 如果已经启动QQ分享，又再次回到onStart，说明分享过程应该已经结束了，这里等待1秒钟后判断是否已经finish，
-        // 如果已经finish，说明Activity收到了QQ的回调，不需要在执行操作了
-        // 如果没有，则需要手动将QQ的AssitActivity结束掉
-        if (mIsQQStart) {
-            getWindow().getDecorView().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    if (!isFinishing()) {
-                        finishAssitActivity();
-                    }
-                }
-            }, 1000);
-            mIsQQStart = false;
-        }
-        super.onStart();
-    }
-
-    private void finishAssitActivity() {
-        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        // 在Android 21以下版本中ActivityManager.getRunningTasks()是可以使用的，
-        // 在21及以上版本中此api被标记为了deprecated，不过仍然可以获取部分信息。
-        // 在21以上版本中可以用21以下版本中ActivityManager.getAppTasks()这个新的api来获取，
-        // 但是在21-23版本中，这个api获取到的AppTask中没有得到activity信息
-        // 所以在21-23版本仍然用原先的ActivityManager.getRunningTasks() api
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            List<ActivityManager.RunningTaskInfo> appTasks = activityManager.getRunningTasks(10);
-            for (ActivityManager.RunningTaskInfo task : appTasks) {
-                if (task.topActivity.getClassName().equals("com.tencent.connect.common.AssistActivity")) {
-                    startAssitActivity();
-                    break;
-                }
-            }
-        } else {
-            List<ActivityManager.AppTask> appTasks = activityManager.getAppTasks();
-            for (ActivityManager.AppTask task : appTasks) {
-                if (task.getTaskInfo().topActivity.getClassName().equals("com.tencent.connect.common.AssistActivity")) {
-                    startAssitActivity();
-                    break;
-                }
-            }
-        }
-    }
-
-    // 由于AssistActivity仍然在当前Acitivity堆栈中，重新start该Activity会触发该Activity的onNewIntent方法
-    // 在onNewIntent中会把AssistActivity finish掉
-    // 事实上，AssistActivity正常返回时也是通过onNewIntent来实现的
-    private void startAssitActivity() {
-        Intent intent = new Intent(QQShareActivity.this, AssistActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
-        mCancelAnyway = true;
-    }
-
-    @Override
-    protected void onResume() {
-        if (!mIsQQStart) {
-            shareToQQ(this, mShareTitle, mShareDetail, mShareImageFile, mShareURL, mIsTimeLine);
-            mIsQQStart = true;
-        }
-        super.onResume();
+        shareToQQ(this, mShareTitle, mShareDetail, mShareImageFile, mShareURL, mIsTimeLine);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (mCancelAnyway) {
-            if (mQQShareListener != null) {
-                mQQShareListener.onCancel();
-            }
-            mCancelAnyway = false;
-        } else {
-            if (mTencent != null) {
-                Tencent.onActivityResultData(requestCode, resultCode, data, mQQShareListener);
-            }
+        if (mTencent != null) {
+            Tencent.onActivityResultData(requestCode, resultCode, data, mQQShareListener);
         }
     }
 
